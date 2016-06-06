a604b5a1b5c407230aaf9c3daa8f0e05bdca25d4
PookaMustard <pooka251@gmail.com>
2016-06-06 22:47:23 +0300

    Update intercommands.py

--- intercommands/intercommands.py
+++ intercommands/intercommands.py
@@ -2,18 +2,18 @@ import discord
 from discord.ext import commands 
  
 class intercommands: 
-     """Random quotes!""" 
+     """Random interactive commands!""" 
  
      def __init__(self, bot): 
          self.bot = bot 
  
      @commands.command(pass_context=True)
      async def riprespect(self, ctx): 
-         """No. Ice."""
+         """Pay your respects. Or not."""
          message = ctx.message
  
          #Your code will go here 
-         await self.bot.say("Press F to pay respects.")
+         await self.bot.say("Send \"F\" to pay respects.")
          response = await self.bot.wait_for_message(author=message.author)
          if response.content.lower().strip() == "f":
               await self.bot.say("Rest in Peace.")
