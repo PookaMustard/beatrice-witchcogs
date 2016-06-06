2ca0ee4830a7a9e11fd3327bd405248645f8feec
PookaMustard <pooka251@gmail.com>
2016-06-06 22:02:08 +0300

    Update randquotes.py

--- randquotes/randquotes.py
+++ randquotes/randquotes.py
@@ -1,7 +1,7 @@
- import discord 
- from discord.ext import commands 
+import discord 
+from discord.ext import commands 
  
- class randquotes: 
+class randquotes: 
      """Random quotes!""" 
  
      def __init__(self, bot): 
