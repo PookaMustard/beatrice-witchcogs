f8a65e91cfc2a13fe655d9605fdae64f4c3b8450
PookaMustard <pooka251@gmail.com>
2016-06-06 22:21:46 +0300

    Add files via upload

---
+++ echo.py
@@ -0,0 +1,22 @@
+import discord
+from cogs.utils import checks
+from bs4 import BeautifulSoup
+import aiohttp
+from discord.ext import commands
+
+class echo:
+    """I'll repeat what you said"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @checks.is_owner()
+    async def echo(self, text):
+        """I'll repeat what you said."""
+
+        #Your code will go here
+        await self.bot.say(text)
+
+def setup(bot):
+    bot.add_cog(echo(bot))
