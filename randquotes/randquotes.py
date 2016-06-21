f9f73fcc60999e19972b26e790dbdd5f4c8a9108
PookaMustard <pooka251@gmail.com>
2016-06-21 17:54:39 +0300

    Update randquotes.py

--- randquotes/randquotes.py
+++ randquotes/randquotes.py
@@ -15,6 +15,17 @@ class randquotes:
          await self.bot.say("In order to use Steam, you have to sign yourself into a contract which basically says \"YOU OWN NOTHING.\" The end result is that you end up buying licenses to games, which they can revoke anytime they wanted. They require the internet for the validation of licenses, which shouldn't be a requirement to play single player games. This results in an ecosystem that basically controls the titles you owned and gives you none of it, forcing you to stay into the ecosystem through the client to play the games you bought even if you don't want the client.")
          await self.bot.say("The best part about all of this is that they can change their contract terms and retroactively apply it onto all the games bought under the previous term. If you don't accept the changes, your account is terminated along with the licenses you've bought under the old terms. You only get to keep the licenses you bought under the old terms IF you agree to them retroactively applying the new terms onto them.")
          await self.bot.say("The end.")
+         
+     @commands.command(pass_context=True) 
+     async def mn9(self, *ctx): 
+         """Mighty No. 9: Thoughts"""
+         server = ctx.message.server
+         if server.id==174932651201921024:
+            break
+         else:
+             return await self.bot.say("Test.")
+#         mn9 = ["MIGHTY NO. 9 IS GETTING MIGHTIED OUTTA THE PLANET.", "Mighty No. 9? My pizza is late!!", "Mighty No. 9? How *unmighty*.",
+#                "A thousand years would pass and still Mighty No. 9 would be quite the laughing stock...don't you think?", "Even Lambdadelta hates Mighty No. 9.", 
 
 def setup(bot):
     bot.add_cog(randquotes(bot))
