2a0f36ad966d2fcd8d2abe13cd03e923dbf10e01
PookaMustard <pooka251@gmail.com>
2016-07-03 09:01:24 +0300

    Create responses.py

---
+++ responses/responses.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from random import randint
+from random import choice as randchoice
+import time
+
+class sao:
+    """Positive response to SAO comments"""
+    
+    def __init__(self, bot):
+        self.bot = bot
+        self.timecheck = {}
+        self.cooldown = 60*3 #60 seconds times the required minutes
+
+    async def check_sao(self, message):
+        if "sao" in message.content.split() and message.author.id != self.bot.user.id:
+            if message.channel.id not in self.timecheck or (time.time() - self.timecheck[message.channel.id]) > self.cooldown:
+                self.timecheck[message.channel.id] = time.time()
+                sao = ["SAO? Why don't you read Umineko no Naku Koro ni instead?", "I won't mute you for five minutes for mentioning SAO.",
+                    "On another server, you'd be muted for mentioning SAO. Talk about it a lot here!"]
+                await self.bot.send_message(message.channel, randchoice(sao))
+
+def setup(bot):
+    n = sao(bot)
+    bot.add_listener(n.check_sao, "on_message")
+    bot.add_cog(n)
