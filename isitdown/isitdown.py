bf3d7374710fae9c46496bc57bd50caf8658b5fb
RaceProUK <raceprouk@hotmail.co.uk>
2016-07-21 18:18:21 +0000

    Improved %isitdown

--- isitdown/isitdown.py
+++ isitdown/isitdown.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 import requests
 from discord.ext import commands
@@ -13,27 +14,23 @@ class isitdown:
     @commands.command()
     async def isitdown(self, url):
         """Checks if a website is down or up."""
-        if url=="":
-                await self.bot.say("You haven't entered a website to check.")
-        if "http://" in url:
-                x=2
-        elif "https://" in url:
-                x=3
-        else:
-                url ="http://" + url
+        if url == "":
+            await self.bot.say("You haven't entered a website to check.")
+            return
+        if "http://" not in url or "https://" not in url:
+            url = "http://" + url
         try:
-                while True:
-                        response = requests.get(url, headers={'user_agent': headers}, timeout=15)
-                        if response.status_code==200:
-                                    await self.bot.say("The website is up and running.")
-                                    break
-                        await self.bot.say("The website is down.")
-                        break
+            loop = asyncio.get_event_loop()
+            response = await loop.run_in_executor(None, requests.get, url, headers = { 'user_agent': headers }, timeout = 15)
+            if response.status_code == 200:
+                await self.bot.say(url + " is up and running.")
+            else
+                await self.bot.say(url + " is down.")
         except requests.exceptions.Timeout:
-                await self.bot.say("The website is down.")
+            await self.bot.say(url + " is down.")
         except requests.exceptions.ConnectionError:
-                await self.bot.say("The website is down.")
-        
+            await self.bot.say(url + " is down.")
+            
 
 def setup(bot):
     bot.add_cog(isitdown(bot))
