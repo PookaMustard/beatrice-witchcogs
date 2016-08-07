6c1022985bec9779ebc3690f36aa7bb6c24806d4
Certainty Witch <pooka251@gmail.com>
2016-08-07 13:08:59 +0200

    %chara now tests from 1000 integers as opposed to 100.

--- randimages/randimages.py
+++ randimages/randimages.py
@@ -78,10 +78,10 @@ class randimages:
     @commands.command()
     async def chara(self):
         """Now this face is good. = )"""
-        chance = randint(1,100)
-        if chance <= 75:
+        chance = randint(1,1000)
+        if chance <= 750:
             return await self.bot.say('=)\nhttp://imgur.com/download/DJGxtu7')
-        elif chance <= 95:
+        elif chance <= 950:
             return await self.bot.say(':)\nhttp://imgur.com/download/DqPfvHB')
         else:
             return await self.bot.say('https://images-1.discordapp.net/.eJwVx10OwiAMAOC7cABqt_KzXcY0DIGEyUKrL8a7G7-372Nes5vdVNVLdoCjSRrzsKJjcsm2jFF65quJTeMEVuVUz_xUAdxwCX7FiCFuK4VIsNzcf-S8jw4xEEF-t55F76nyZFvaw3x_44IlKg.u4gRjc9h4xSj0qE6LRLG_KtxQDA.gif')
