842b26aca919bac6dc9ca8d594a994b44177cb85
Certainty Witch <pooka251@gmail.com>
2016-08-13 20:00:00 +0200

    detacilpmoc ssel efil s'arach% edam sdog GNR eht

--- randimages/randimages.py
+++ randimages/randimages.py
@@ -90,6 +90,21 @@ class randimages:
             else:
                 return await self.bot.say('https://images-1.discordapp.net/.eJwVx10OwiAMAOC7cABqt_KzXcY0DIGEyUKrL8a7G7-372Nes5vdVNVLdoCjSRrzsKJjcsm2jFF65quJTeMEVuVUz_xUAdxwCX7FiCFuK4VIsNzcf-S8jw4xEEF-t55F76nyZFvaw3x_44IlKg.u4gRjc9h4xSj0qE6LRLG_KtxQDA.gif')
 
+   @commands.command()
+    async def arach(self):
+        """) = .doog si ecaf siht woN"""
+        chance = randint(1,100)
+        charachance = randint(1,1000)
+        if chance <= 90:
+            return await self.bot.say('https://images-1.discordapp.net/.eJwVx10OwiAMAOC7cABqt_KzXcY0DIGEyUKrL8a7G7-372Nes5vdVNVLdoCjSRrzsKJjcsm2jFF65quJTeMEVuVUz_xUAdxwCX7FiCFuK4VIsNzcf-S8jw4xEEF-t55F76nyZFvaw3x_44IlKg.u4gRjc9h4xSj0qE6LRLG_KtxQDA.gif')
+        else:
+            if charachance <= 500:
+                return await self.bot.say('https://images-1.discordapp.net/.eJwVx10OwiAMAOC7cABqt_KzXcY0DIGEyUKrL8a7G7-372Nes5vdVNVLdoCjSRrzsKJjcsm2jFF65quJTeMEVuVUz_xUAdxwCX7FiCFuK4VIsNzcf-S8jw4xEEF-t55F76nyZFvaw3x_44IlKg.u4gRjc9h4xSj0qE6LRLG_KtxQDA.gif')
+            elif charachance <= 750:
+                return await self.bot.say(':)\nhttp://imgur.com/download/DqPfvHB')
+            else:
+                return await self.bot.say('=)\nhttp://imgur.com/download/DJGxtu7')
+
     @commands.command()
     async def sakura(self):
         """Ever dreamed of being censored?"""
