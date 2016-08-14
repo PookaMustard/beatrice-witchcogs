aced3cb1319ff0b97658aeb6d22f19e4d114fb1c
r543 <r543@windowslive.com>
2016-08-14 17:12:43 +0200

    Update randquotes.py

--- randquotes/randquotes.py
+++ randquotes/randquotes.py
@@ -13,7 +13,7 @@ class randquotes:
 
     @commands.command()
     async def steamrant(self): 
-        """No. Ice.""" 
+        """Yes. Water.""" 
         await self.bot.say("In order to use Steam, you have to sign yourself into a contract which basically says \"YOU OWN NOTHING.\" The end result is that you end up buying licenses to games, which they can revoke anytime they wanted. They require the internet for the validation of licenses, which shouldn't be a requirement to play single player games. This results in an ecosystem that basically controls the titles you owned and gives you none of it, forcing you to stay into the ecosystem through the client to play the games you bought even if you don't want the client.")
         await self.bot.say("The best part about all of this is that they can change their contract terms and retroactively apply it onto all the games bought under the previous term. If you don't accept the changes, your account is terminated along with the licenses you've bought under the old terms. You only get to keep the licenses you bought under the old terms IF you agree to them retroactively applying the new terms onto them.")
         await self.bot.say("The end.")
