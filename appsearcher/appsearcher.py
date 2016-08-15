3405c69b6853b1da7a08dd2bea0316ef21647dc0
Certainty Witch <pooka251@gmail.com>
2016-08-15 16:17:20 +0200

    Update appsearcher.py

--- appsearcher/appsearcher.py
+++ appsearcher/appsearcher.py
@@ -30,11 +30,11 @@ class appsearcher:
             except:
                 return await self.bot.say("Couldn't load amount of DRM-free games on GOG. There must be an error.")
         else:
-            try:
+            if text[0]!='random':
                 text = " ".join(text)
                 text = text.replace(" ", "%20")
                 query = 'https://www.gog.com/games/ajax/filtered?limit=5&search=' + text
-            except IndexError:
+            else:
                 query='https://www.gog.com/games/ajax/filtered?limit=99999'
             #Loading ajax search URL into variable r
             r = requests.get(query)
