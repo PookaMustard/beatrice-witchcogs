e5a63a83fa0680ef91675c95dd83f7c292b0332f
Certainty Witch <pooka251@gmail.com>
2016-08-18 18:48:30 +0200

    Update appsearcher.py

--- appsearcher/appsearcher.py
+++ appsearcher/appsearcher.py
@@ -15,9 +15,10 @@ class appsearcher:
         self.bot = bot
 
     @commands.command()
-    async def gog(self, *text):
+    async def gog(self, ctx, *text):
         """Returns GOG search results using game name
             Type %gog gamecount for the number of games"""
+        message = ctx.message
 
         #Your code will go here
         if text[0]=='gamecount':
