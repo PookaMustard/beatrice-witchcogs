28297eec5ed428a974a98cc8a2e889ef3a96e780
Certainty Witch <pooka251@gmail.com>
2016-08-20 15:57:11 +0200

    Added the cog

---
+++ thepiratebay/thepiratebay.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+class thepiratebay:
+    """Search for files on thepiratebay.org"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def tpb(self, *text):
+        """Returns TPB torrent search results using item name"""
+
+        #Your code will go here
+        text = " ".join(text)
+        query=text.replace(".", "")
+        query=query.replace(" ", "%20")
+        await self.bot.say("***WARNING:*** May or may not include NSFW content\nhttps://thepiratebay.org/search/" + query + "/0/99/0")
+
+def setup(bot):
+    bot.add_cog(thepiratebay(bot))
