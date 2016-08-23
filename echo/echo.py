0558ccbf1b08597d12c4cb8ffb1c2aa9f1cccc96
Certainty Witch <pooka251@gmail.com>
2016-08-23 20:52:42 +0200

    Now using more efficient and friendlier code.

--- echo/echo.py
+++ echo/echo.py
@@ -1,35 +1,8 @@
+from cogs.utils import checks
 import discord
 from discord.ext import commands
-from cogs.utils import checks
-from __main__ import set_cog, send_cmd_help, settings
-import importlib
-import traceback
-import logging
-import asyncio
-import threading
-import datetime
-import glob
-import os
-import time
 
-log = logging.getLogger("red.owner")
-
-class CogNotFoundError(Exception):
-    pass
-
-class CogLoadError(Exception):
-    pass
-
-class NoSetupError(CogLoadError):
-    pass
-
-class CogUnloadError(Exception):
-    pass
-
-class OwnerUnloadWithoutReloadError(CogUnloadError):
-    pass
-
-class echo:
+class Echo:
     """I'll repeat what you said."""
 
     def __init__(self, bot):
@@ -40,36 +13,18 @@ class echo:
     async def echo(self, *text):
         """I'll repeat what you said."""
 
-        #Your code will go here
         text = " ".join(text)
         await self.bot.say(text)
 
     @commands.command()
     @checks.is_owner()
-    async def sonar(self, serverid, *, text):
+    async def sonar(self, channelid, *, text):
         """I'll repeat what you said and where you want it.
-        
         A modified version of the debug command, with help from Calebj."""
 
-        #Your code will go here
-        text = "".join(text)
         text = text.replace("\'", "\\\'")
-        local_vars = locals().copy()
-        local_vars['bot'] = self.bot
-        code = "bot.send_message(bot.get_channel(serverid),'"+text+"')"
-        python = '```py\n{}\n```'
-        result = None
-
-        try:
-            result = eval(code, globals(), local_vars)
-        except Exception as e:
-            await self.bot.say(python.format(type(e).__name__ + ': ' + str(e)))
-            return
-                    
-        if asyncio.iscoroutine(result):
-            result = await result
-
-        result = python.format(result)
+        channelid = self.bot.get_channel(str(channelid))
+        return await self.bot.send_message(channelid, text)
 
     @commands.command(pass_context=True)
     @checks.is_owner()
@@ -79,4 +34,4 @@ class echo:
         return await self.bot.say(channel.id)
 
 def setup(bot):
-    bot.add_cog(echo(bot))
+    bot.add_cog(Echo(bot))
