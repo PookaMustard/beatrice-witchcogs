8e7f4e12fd0c977325c26a160e87b69a603a9950
Certainty Witch <pooka251@gmail.com>
2017-01-10 14:22:57 +0200

    Major update
    +No longer produces a search URL only. It also crawls through The Pirate Bay's search engine to find torrents, choosing one in the form of an embed.
    +If there is only one result, the bot will send a link to that result directly along with a basic description of the torrent.
    +If there are many results, the bot will ask you which one out of five that you want.
    +If there are more than five results, the bot will produce a search URL to view more torrents.
    *Requires Beautiful Soup 4 to run this version.

--- thepiratebay/thepiratebay.py
+++ thepiratebay/thepiratebay.py
@@ -1,21 +1,91 @@
 import discord
 from discord.ext import commands
+import requests
+try:
+    from bs4 import BeautifulSoup
+    bs4ava = True
+except:
+    bs4ava = False
+
 
 class ThePirateBay:
-    """Search for files on thepiratebay.org"""
+    """Search for files on the Pirate Bay"""
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def tpb(self, *text):
-        """Returns TPB torrent search results using item name"""
+    @commands.command(pass_context=True)
+    async def tpb(self, ctx, *, text):
+        """Returns Pirate Bay torrent search results using item name"""
 
-        #Your code will go here
-        text = " ".join(text)
-        query=text.replace(".", "")
-        query=query.replace(" ", "%20")
-        await self.bot.say("***WARNING:*** May or may not include NSFW content\nhttps://thepiratebay.org/search/" + query + "/0/99/0")
+        # Initializing variables and URLs, loading search into memory
+        message = ctx.message
+        initm = '```'
+        url = 'http://thepiratebay.org'
+        searchurl = url + '/search/{}/0/99/0'.format(text.replace(" ", "%20"))
+        search = requests.get(searchurl).content
+        soup = BeautifulSoup(search)
+
+        # Searching for all torrent links found on the search page
+        var = soup.find_all(class_='detLink')
+
+        # Identifying amount of results, determining the message to send users
+        # First clause will send a message for no results
+        # Second clause will pick the only search result for usage
+        # Third clause will ask the user which of the results to choose from
+        # If there are more than five results, send the search URL
+        # In cases of found results, link, title and description extracted
+        if len(var) == 0:
+            return await self.bot.say("No results found.")
+        elif len(var) == 1:
+            title = var[0].get_text()
+            finalurl = str(url) + str(var[0].get('href'))
+            desc = soup.find(class_='detDesc').get_text()
+        else:
+            for count, i in enumerate(var):
+                if count > 4:
+                    count = 4
+                    break
+                numcount = count + 1
+                initm = initm + str(numcount) + ". " + \
+                    var[count].get_text() + "\n"
+            initm = initm + '```'
+            if len(var) > 4:
+                initm = initm + "More results found here: <" + \
+                    searchurl + ">\n"
+            initm = initm + "Please type the number of the item you want." + \
+                " Type `cancel` to cancel the search."
+            await self.bot.say(initm)
+            rp = await self.bot.wait_for_message(
+                author=message.author, channel=message.channel)
+            try:
+                index = int(rp.content) - 1
+                if (index >= count + 1) or (index < 0):
+                    await self.bot.say("Chosen number invalid. " +
+                                       "Assuming first search " +
+                                       "result.")
+                    index = 0
+            except:
+                if str(rp.content).lower() == 'cancel':
+                    return await self.bot.say("Search cancelled.")
+                await self.bot.say("Cannot accept strings for " +
+                                   "choosing search results. " +
+                                   "Assuming first search result.")
+                index = 0
+            title = var[index].get_text()
+            finalurl = str(url) + str(var[index].get('href'))
+            desc = soup.find_all(class_='detDesc')[index * 2].get_text()
+
+        # Preparing information to send in a Discord embed
+        desc = desc + "\n***WARNING:*** May or may not include NSFW content"
+        em = discord.Embed(title=title, url=finalurl, description=desc)
+
+        # Finally sending the message
+        return await self.bot.say(embed=em)
+
 
 def setup(bot):
-    bot.add_cog(ThePirateBay(bot))
+    if not bs4ava:
+        raise RuntimeError("You need to run 'pip install bs4' to run the cog")
+    else:
+        bot.add_cog(ThePirateBay(bot))
